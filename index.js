934baf11ca117971882f4884f18405675341f1fb
mustafa siam <mustafasiam357@gmail.com>
2025-08-25 18:54:27 +0600

    carts post get added

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 //middleware
-app.use(cors())
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+    ],
+    credentials: true
+}))
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ejjba9r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -26,12 +31,22 @@ async function run() {
     // Send a ping to confirm a successful connection
     const menucollection=client.db('BistroDB').collection('menu')
     const reviwcollection=client.db('BistroDB').collection('reviews')
+    const cartcollection=client.db('BistroDB').collection('carts')
     app.get('/menu',async(req,res)=>{
       const result=await menucollection.find().toArray();
       res.send(result)
     })
     app.get('/review',async(req,res)=>{
       const result=await reviwcollection.find().toArray();
+      res.send(result)
+    })
+    app.get('/carts',async(req,res)=>{
+      const result=await cartcollection.find().toArray();
+      res.send(result)
+    })
+    app.post('/carts',async(req,res)=>{
+      const cart=req.body;
+      const result=await cartcollection.insertOne(cart);
       res.send(result)
     })
     await client.db("admin").command({ ping: 1 });
